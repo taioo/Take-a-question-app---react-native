1a5d39b04d84fa13e254cb893e294db4d87a2c34
taioo <thaer.tai@gmail.com>
2019-02-26 10:48:30 +0100

    add model

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
-const listItem = (props) => {
-    return (
-        <TouchableOpacity onPress={props.onItemPressed}>
-            <View style={styles.listItem}>
-                <Text>{props.placeName}</Text>
-            </View>
-        </TouchableOpacity>
-    );
-}
+const listItem = props => (
+  <TouchableOpacity onPress={props.onItemPressed}>
+    <View style={styles.listItem}>
+      <Image resizeMode="cover" source={props.placeImage} style={styles.placeImage} />
+      <Text>{props.placeName}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
-    listItem: {
-        width: "100%",
-        marginBottom: 5,
-        padding: 10,
-        backgroundColor: "#eee"
-    }
+  listItem: {
+    width: "100%",
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  placeImage: {
+      marginRight: 8,
+      height: 30,
+      width: 30
+  }
 });
 
 export default listItem;
